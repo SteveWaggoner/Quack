35f826cf4dba4aa23959a52a301c05c111788560
Sam Waggoner <samkwaggoner@gmail.com>
2022-12-04 16:35:50 -0800

    Sam changes

--- update_maps.sh
+++ update_maps.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Pack maps
-./q1k3-master/q1k3-master/pack_map.exe /cygdrive/C/tmp/m1.map /tmp/m1.plb
-./q1k3-master/q1k3-master/pack_map.exe /cygdrive/C/tmp/m2.map /tmp/m2.plb
+./q1k3-master/q1k3-master/pack_map.exe /tmp/m1.map /tmp/m1.plb
+./q1k3-master/q1k3-master/pack_map.exe /tmp/m2.map /tmp/m2.plb
 
 # Concat all maps into one file
 cat \
