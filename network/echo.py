dd97b82230abe6da9f13ee9c1e01a8dd79183303
Steve <stwaggoner@gmail.com>
2023-04-03 06:40:04 +0000

    fixed

--- network/echo.py
+++ network/echo.py
@@ -2,23 +2,23 @@ from flask import Blueprint, render_template
 from flask_sock import Sock
 
 sock = Sock()
-echo = Blueprint('echo', __name__,
+echo_bp = Blueprint('echo', __name__,
     template_folder='templates',
     static_folder='static', 
     static_url_path='assets')
 
 def register_page(app):
-    app.register_blueprint(echo, url_prefix='/echo')
+    app.register_blueprint(echo_bp, url_prefix='/echo')
     sock.init_app(app)
 
 
-@echo.route('/')
+@echo_bp.route('/')
 def index():
     return render_template('index.html')
 
 
 
-@sock.route('/ws',bp=echo)
+@sock.route('/ws',bp=echo_bp)
 def echo(sock):
     while True:
         data = sock.receive()
